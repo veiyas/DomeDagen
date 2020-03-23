124259e8317844f2101e80b5ee09f9a09666e48c
veiyas <david.rk97@gmail.com>
2020-03-23 16:55:36 +0100

    Added a simple cube and I/O for movement to properly test fish eye projection

--- src/shaders/testing_vertex.glsl
+++ src/shaders/testing_vertex.glsl
@@ -5,10 +5,11 @@ layout(location = 1) in vec3 vertColor;
 
 uniform mat4 mvp;
 uniform mat3 transformation;
+uniform mat4 transformation2;
 
 out vec3 fragColor;
 
 void main() {
 	fragColor = vertColor;
-	gl_Position = mvp * vec4(transformation * vertPosition, 1.f);	
+	gl_Position = mvp * transformation2 * vec4(transformation * vertPosition, 1.f);	
 }
