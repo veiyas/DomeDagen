07d5610b3f95bb6bd21b2234561ec1ec273b4c6a
Frans Johansson <fransjohansson98@gmail.com>
2020-04-07 11:10:56 +0200

    Add models: bottles, cans and six pack plastic

--- src/game.hpp
+++ src/game.hpp
@@ -23,7 +23,7 @@
 #include "utility.hpp"
 
 //Temp name holders for easier file reading
-const std::vector<std::string> allModelNames{ "fish" };
+const std::vector<std::string> allModelNames{ "fish", "can1", "can2", "can3", "can4", "bottle1", "bottle2", "bottle3", "sixpack1", "sixpack2", "sixpack3" };
 const std::vector<std::string> allShaderNames{ "player", "testing", "sceneobject" };
 
 // abock;  consider implementing all of this as an "implicit" singleton.  Instead of
