829d807d87b116bea9af4f31b7479928bf4a99a6
Frans Johansson <fransjohansson98@gmail.com>
2020-04-14 18:32:53 +0200

    Render players in their given colours using the mask texture

--- src/shaders/playerfrag.glsl
+++ src/shaders/playerfrag.glsl
@@ -8,15 +8,20 @@ in vec3 interpolatedNormal;
 
 in vec3 light;
 
+// frans; Playing with setting colors through uniforms
+uniform vec3 primaryCol;
+uniform vec3 secondaryCol;
+
 out vec4 color;
 
 void main() {
-     vec4 texcolor = texture(tex, st);
+     //vec4 texcolor = texture(tex, st);
+     vec4 mask = texture(tex, st);
      vec3 nNormal = normalize(interpolatedNormal);
      float diffuse = max(0.0, dot(nNormal, light));
 
      //Only ambient light for now
-     color = texcolor;
+     //color = texcolor;
 
-//     color = vec4(1.0, 1.0, 1.0, 1.0);
+     color = vec4(primaryCol, 1.0) * (1.0 - mask) + mask * vec4(secondaryCol, 1.0);
 }
