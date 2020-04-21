7dfd47be2d1efa33be865091b48b6d129a82319c
Frans Johansson <fransjohansson98@gmail.com>
2020-04-21 11:43:58 +0200

    Set shader parameters as preset uniform variables

--- src/shaders/playerfrag.glsl
+++ src/shaders/playerfrag.glsl
@@ -11,6 +11,14 @@ in vec3 light;
 
 out vec4 color;
 
+// Preset shader parameters. Can be modified from the game logic as well.
+uniform float diffuseStrength = 0.25;
+uniform vec4 diffuseColour = vec4(1.0, 1.0, 1.0, 1.0);
+uniform float ambientStrength = 0.75;
+uniform float shininess = 25;
+uniform float specularStrength = 0.8;
+uniform vec4 specularColour = vec4(1.0, 1.0, 1.0, 1.0);
+
 void main() {
     // Normalized normal, light reflection and "view" vectors
     vec3 nNormal = normalize(interpolatedNormal);
@@ -21,19 +29,11 @@ void main() {
     float mask = vec4(texture(tex, st)).r;
     vec4 albedo = (1.0 - mask) * vec4(primaryCol, 1.0) + mask * vec4(secondaryCol, 1.0);
 
-    // Ambient light
-    float ambientStrength = 0.75;
-
     // Diffuse light
     float diffuseAmount = max(0.0, dot(nNormal, light));
-    float diffuseStrength = 0.25;
-    vec4 diffuseColour = vec4(1.0, 1.0, 1.0, 1.0);
 
     // Specular light (only on masked areas)
-    float shininess = 25;
     float specularAmount = mask * pow(max(0.0, dot(rLight, view)), shininess);
-    float specularStrength = 0.8;
-    vec4 specularColour = vec4(1.0, 1.0, 1.0, 1.0);
-
+    
     color = albedo * ambientStrength + diffuseAmount * diffuseStrength * diffuseColour + specularAmount * specularStrength * specularColour;
 }
