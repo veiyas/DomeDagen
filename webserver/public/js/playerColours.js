24090e290169b7693f871eb2cae4f25c5fa21dd7
Frans Johansson <fransjohansson98@gmail.com>
2020-04-29 17:32:55 +0200

    Tweaks to game page. Show player colours in right corner

--- webserver/public/js/playerColours.js
+++ webserver/public/js/playerColours.js
@@ -1,10 +1,16 @@
 function setPlayerColours(primary, secondary) {
     var svg = document.getElementById('playerCharacterSVG');
-    var primaryElement = svg.getElementById('Body');
+    var bodyElement = svg.getElementById('Body');
     var detailElement = svg.getElementById('Details');
     var ringElement = svg.getElementById('Ring').children[0];
 
-    primaryElement.style.fill = "rgb(" + primary + ")";
+    bodyElement.style.fill = "rgb(" + primary + ")";
     detailElement.style.fill = "rgb(" + secondary + ")";
     ringElement.style.fill = "rgb(" + primary + ")";
+
+    // Set colours on game running page as well
+    var primaryElement = document.querySelector('#primaryColour');
+    var secondaryElement = document.querySelector('#secondaryColour');
+    primaryElement.style.backgroundColor = "rgb(" + primary + ")";
+    secondaryElement.style.backgroundColor = "rgb(" + secondary + ")";
 }
