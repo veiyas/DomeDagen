043a13f689458d6dacd6e18d4311ea9b55a86ecf
David <david.rk97@gmail.com>
2020-05-19 09:58:14 +0200

    Fixed for dome testing

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
 	Configuration config = sgct::parseArguments(arg);
 
 	//Choose which config file (.xml) to open
-	config.configFilename = rootDir + "/src/configs/fisheye_testing.xml";
+	//config.configFilename = rootDir + "/src/configs/fisheye_testing.xml";
 	//config.configFilename = rootDir + "/src/configs/simple.xml";
 	//config.configFilename = rootDir + "/src/configs/six_nodes.xml";
 	//config.configFilename = rootDir + "/src/configs/two_fisheye_nodes.xml";
@@ -181,13 +181,13 @@ void initOGL(GLFWwindow*)
 	/**********************************/
 	/*			 Debug Area			  */
 	/**********************************/
-	if (Engine::instance().isMaster())
-	{
-		for (size_t i = 0; i < 10; i++)
-		{
-			Game::instance().addPlayer(glm::vec3(0.f + 0.3f * i));
-		}
-	}
+	//if (Engine::instance().isMaster())
+	//{
+	//	for (size_t i = 0; i < 10; i++)
+	//	{
+	//		Game::instance().addPlayer(glm::vec3(0.f + 0.3f * i));
+	//	}
+	//}
 }
 
 void keyboard(Key key, Modifier modifier, Action action, int)
